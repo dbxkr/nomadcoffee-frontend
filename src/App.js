c01a702e6fbb44311c2e629667aef428f3fcd0b5
Jeon <btt0408@gmail.com>
2023-01-25 01:45:05 +0900

    1.1

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
-import { useReactiveVar } from "@apollo/client";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ApolloProvider, useReactiveVar } from "@apollo/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 import NotFound from "./screens/NotFound";
-import { darkModeVar, isLoggedInVar } from "./apollo";
+import { darkModeVar, isLoggedInVar, client } from "./apollo";
 import { ThemeProvider } from "styled-components";
 import { darkTheme, GlobalStyles, lightTheme } from "./styles";
+import SignUp from "./screens/SignUp";
+import routes from "./routes";
+import { HelmetProvider } from "react-helmet-async";
 
 function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
   const darkMode = useReactiveVar(darkModeVar);
   return (
-    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-      <GlobalStyles />
-      <Router>
-        <Switch>
-          <Route path="/" exact>
-            {isLoggedIn ? <Home /> : <Login />}
-          </Route>
-          <Route>
-            <NotFound />
-          </Route>
-        </Switch>
-      </Router>
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <HelmetProvider>
+        <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+          <GlobalStyles />
+          <Router>
+            <Routes>
+              <Route
+                path={routes.home}
+                element={isLoggedIn ? <Home /> : <Login />}
+              />
+              {!isLoggedIn ? (
+                <Route path={routes.signUp} element={<SignUp />} />
+              ) : null}
+              <Route element={<NotFound />} />
+            </Routes>
+          </Router>
+        </ThemeProvider>
+      </HelmetProvider>
+    </ApolloProvider>
   );
 }
 
---
+++ src/screens/SignUp.js
@@ -0,0 +1,134 @@
+import { gql, useMutation } from "@apollo/client";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import AuthLayout from "../components/auth/AuthLayout";
+import BottomBox from "../components/auth/BottomBox";
+import Button from "../components/auth/Button";
+import FormBox from "../components/auth/FormBox";
+import Input from "../components/auth/Input";
+import PageTitle from "../components/PageTitle";
+import { FatLink } from "../components/shared";
+import routes from "../routes";
+
+const HeaderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Subtitle = styled(FatLink)`
+  font-size: 16px;
+  text-align: center;
+  margin-top: 10px;
+`;
+
+const CREATE_ACCOUNT_MUTATION = gql`
+  mutation createAccount(
+    $firstName: String!
+    $lastName: String
+    $username: String!
+    $email: String!
+    $password: String!
+  ) {
+    createAccount(
+      firstName: $firstName
+      lastName: $lastName
+      username: $username
+      email: $email
+      password: $password
+    ) {
+      ok
+      error
+    }
+  }
+`;
+
+function SingUp() {
+  const navigate = useNavigate();
+  const onCompleted = (data) => {
+    const { username, password } = getValues();
+    const {
+      createAccount: { ok, error },
+    } = data;
+    if (!ok) {
+      return;
+    }
+    navigate.push(routes.home, {
+      message: "Account created. Please log in.",
+      username,
+      password,
+    });
+  };
+  const [createAccount, { loading }] = useMutation(CREATE_ACCOUNT_MUTATION, {
+    onCompleted,
+  });
+  const { register, handleSubmit, formState, getValues } = useForm({
+    mode: "onChange",
+  });
+  const onSubmitValid = (data) => {
+    if (loading) {
+      return;
+    }
+    createAccount({
+      variables: {
+        ...data,
+      },
+    });
+  };
+  return (
+    <AuthLayout>
+      <PageTitle title="Sign up" />
+      <FormBox>
+        <HeaderContainer>
+          <FontAwesomeIcon icon={faCoffee} size="3x" />
+          <Subtitle>Sign up to enjoy delicious coffee.</Subtitle>
+        </HeaderContainer>
+        <form onSubmit={handleSubmit(onSubmitValid)}>
+          <Input
+            {...register("firstName", {
+              required: "First Name is required.",
+            })}
+            type="text"
+            placeholder="First Name"
+          />
+          <Input
+            {...register("lastName")}
+            type="text"
+            placeholder="Last Name"
+          />
+          <Input
+            {...register("email", {
+              required: "Email is required.",
+            })}
+            type="text"
+            placeholder="Email"
+          />
+          <Input
+            {...register("username", {
+              required: "Username is required.",
+            })}
+            type="text"
+            placeholder="Username"
+          />
+          <Input
+            {...register("password", {
+              required: "Password is required.",
+            })}
+            type="password"
+            placeholder="Password"
+          />
+          <Button
+            type="submit"
+            value={loading ? "Loading..." : "Sign up"}
+            disabled={!formState.isValid || loading}
+          />
+        </form>
+      </FormBox>
+      <BottomBox cta="Have an account?" linkText="Log in" link={routes.home} />
+    </AuthLayout>
+  );
+}
+export default SingUp;
